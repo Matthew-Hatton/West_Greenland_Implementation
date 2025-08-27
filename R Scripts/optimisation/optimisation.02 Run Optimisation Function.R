76450766e17ed61dd04fa1c9c28e0b1adbd01637
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-08-28 00:57:14 +0100

    second run

--- R Scripts/optimisation/optimisation.02 Run Optimisation Function.R
+++ R Scripts/optimisation/optimisation.02 Run Optimisation Function.R
@@ -6,17 +6,19 @@ library(purrr)
 library(future)
 future:::ClusterRegistry("stop") # make sure all additional clusters are closed
 
+tic()
 n_runs <- availableCores() - 2
 plan(multisession,workers = n_runs)
 
 parallel_optimise <- function(){
   model <- e2ep_read("West_Greenland.test","2011-2019")
-  opt_eco <- e2ep_optimize_eco(model,nyears = 50,n_iter = 3,quiet = F,start_temperature = 1,cooling = 1)
+  opt_eco <- e2ep_optimize_eco(model,nyears = 50,n_iter = 500,quiet = T,start_temperature = 1,cooling = 1)
   return(opt_eco)
 }
 
-opt_eco <- future_map(1:n_runs, ~ parallel_optimise(),.progress = T)
+opt_eco <- future_map(1:n_runs, ~ parallel_optimise(),.progress = F)
 # saveRDS(opt_eco,"./Objects/Optimisation/WG.1000iterV3_no_fishing_parallel.RDS")
 saveRDS(opt_eco,"./Objects/Optimisation/WG.1000ADDITIONALiter.RDS")
 
 future:::ClusterRegistry("stop") # make sure all additional clusters are closed
+toc()
